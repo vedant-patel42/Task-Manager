623287385508c23638c1d07382077d24079e8f3a
vedant_42 <32434874+vedant-patel42@users.noreply.github.com>
2023-01-30 23:55:19 -0500

    Modify schema with adding Task Description

--- models/Task.js
+++ models/Task.js
@@ -7,6 +7,12 @@ const TaskSchema = new mongoose.Schema({
        trim:true,
        maxlength:[20,'name cannot be more than 20 characters']
    },
+   description: {
+    type: String,
+    required: [true,'must provide description'],
+    trim: true,
+    maxlength: [500, 'Describe in no more than 50 characters'],
+  },
    completed:{
        type:Boolean,
        default:false
